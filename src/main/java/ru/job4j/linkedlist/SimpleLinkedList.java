c13e7b076f914cd20aeeb300394f01d5e5ee9de2
Kotov Oleg <grum1972@gmail.com>
2022-02-05 16:44:00 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/linkedlist/SimpleLinkedList.java
@@ -0,0 +1,84 @@
+package ru.job4j.linkedlist;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements List<E> {
+    private static int index;
+    private int size;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount;
+
+    SimpleLinkedList() {
+        index = 0;
+    }
+
+    private static class Node<E> {
+        private int index;
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.index = SimpleLinkedList.index;
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        if (first == null) {
+            Node<E> node = new Node(null, value, null);
+            first = node;
+            last = node;
+        } else {
+            Node<E> node = new Node(last, value, null);
+            last.next = node;
+            last = node;
+        }
+        size++;
+        modCount++;
+        index++;
+    }
+
+    @Override
+    public E get(int index) {
+        Node<E> cursor = first;
+        Objects.checkIndex(index, size);
+        while (cursor.index != index) {
+            cursor = cursor.next;
+        }
+        return cursor.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private Node<E> cursor = first;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("The list has been modified");
+                }
+                return cursor != null;
+            }
+
+            @Override
+            public E next() {
+                E elem;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                elem = cursor.item;
+                cursor = cursor.next;
+                return elem;
+            }
+        };
+    }
+}
