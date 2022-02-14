891b955aa7b942fbf02397ca6dea435447204315
Kotov Oleg <grum1972@gmail.com>
2022-02-14 21:00:43 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    @SuppressWarnings("checkstyle:SimplifyBooleanExpression")
+    public void unavailable(String source, String target) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            String strLog = "";
+            boolean status = true;
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                String[] words = line.split(" ");
+                if (("400".equals(words[0]) || "500".equals(words[0]))) {
+                    if (status) {
+                        strLog = words[1] + ";";
+                        status = false;
+                    }
+                }
+                if ("200".equals(words[0]) || "300".equals(words[0])) {
+                    if (!status) {
+                        strLog += words[1];
+                        out.println(strLog);
+                        status = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy anal = new Analizy();
+        anal.unavailable("server.log", "target.log");
+    }
+}
