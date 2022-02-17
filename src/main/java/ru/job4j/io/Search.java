c17baca19738cee538cf6217a410144233515d07
Kotov Oleg <grum1972@gmail.com>
2022-02-17 22:21:15 +0300

    5. Валидация параметров запуска. [#246865 #265631]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,15 +9,22 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("You need input root folder "
                     + "and file extension for search");
         }
         Path start = Paths.get(args[0]);
         String ext = args[1];
+        if (!start.toFile().isDirectory()) {
+            throw new IllegalArgumentException("You need input root folder first argument");
+        }
+        if (ext.charAt(0) != '.') {
+            ext = "." + args[1];
+        }
+        String finalExt = ext;
         search(start, p -> p.toFile()
                 .getName()
-                .endsWith("." + ext))
+                .endsWith(finalExt))
                 .forEach(System.out::println);
     }
 
