eea8fc530c21d1d2a7b678ab1176b80e19b6ead1
Kotov Oleg <grum1972@gmail.com>
2022-03-10 22:59:00 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,8 +4,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -13,18 +18,28 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine();
-                         str != null && !str.isEmpty();
-                         str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.contains(" /?msg=Bye")) {
-                                server.close();
-                                break;
+                    String str = in.readLine();
+                    if (str.contains("msg")) {
+                        str = str.substring(str.indexOf('=') + 1);
+                        str = str.substring(0, str.indexOf(' '));
+                        if (str.contains("Hello")) {
+                            out.write("Hello!".getBytes());
                         }
+                        if (str.contains("What")) {
+                            out.write("What".getBytes());
+                        }
+                        if (str.contains("Bye")) {
+                            out.write("Bye".getBytes());
+                            server.close();
+                        }
+                        out.flush();
                     }
-                    out.flush();
+                } catch (Exception e) {
+                    LOG.error("Wrong execute", e);
                 }
             }
+        } catch (Exception e) {
+            LOG.error("Wrong execute", e);
         }
     }
 }
