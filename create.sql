7cbd36d13ea9d52365590e0c0546a3a944c390c6
Kotov Oleg <grum1972@gmail.com>
2022-03-17 21:01:11 +0300

    0. Что такое база данных. [#6860]

---
+++ create.sql
@@ -0,0 +1,8 @@
+create table customers
+(
+    id serial primary key,
+    firstName character(30),
+    lastName character(30),
+    email character(30),
+    age integer
+);
---
+++ update.sql
@@ -0,0 +1,1 @@
+update customers set firstname='Ivanov'
